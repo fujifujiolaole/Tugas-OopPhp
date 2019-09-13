1055dd9ef52412c3d7b1d0703d3065d590ccfc5d
fujifujiolaole <fuji.santoso@koding.sch.id>
2019-09-13 07:23:47 +0700

    SetterGetter.php

---
+++ SetterGetter.php
@@ -0,0 +1,31 @@
+<?php
+
+class User
+{
+    public $email;
+
+    public function setEmail($email)
+    {
+        //Validasi
+        if(!filter_var ($email,FILTER_VALIDATE_EMAIL)) 
+        {
+
+        return;
+        }
+     
+    $this->email = $email;
+    }
+
+     
+    public function getEmail()
+    {
+         return strtolower($this->email);
+    }
+
+}
+
+$user = new User;
+$user->setEmail("fuji.santoso@koding.sch.id");
+
+
+echo $user->getEmail();
